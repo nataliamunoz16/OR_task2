d7140e31577e85ff0c78ec8f00e9ac89761decd6
Nerocraft4 <90830812+Nerocraft4@users.noreply.github.com>
2026-03-23 21:43:42 +0000

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-ROOT = Path("/home/natalia/Escritorio/MAI/OR")
-TRAIN_IMG= ROOT/"train2020/train/"
-TEST_IMG =ROOT/"val_test2020/test/"
-TRAIN_MASK = ROOT/"mask_train/"
-TEST_MASK= ROOT/"mask_val/"
+ROOT = Path(r"/mnt/a27b1cbf-298a-4896-8528-84006d36fd9e/Nero/Documents/Acadèmic/UPC - MAMME/OR/P2/OR_task2")
+TRAIN_IMG= ROOT/"coco/images/train/"
+TEST_IMG =ROOT/"coco/images/val/"
+TRAIN_MASK = ROOT/"coco/segmentations/seg_train/"
+TEST_MASK= ROOT/"coco/segmentations/seg_val/"
 MODELS = ROOT/"models/"
 RESULTS = ROOT/"results/"
